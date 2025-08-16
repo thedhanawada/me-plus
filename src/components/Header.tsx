8230c1d0c89ea27aca1c77bc4bd66907add54689
thedhanawada <wep.proto@gmail.com>
2025-08-16 23:04:59 +1000

    feat: update github link to source code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,13 +55,13 @@ const Header = () => {
               })}
               
               <a
-                href="https://github.com/thedhanawada"
+                href="https://github.com/thedhanawada/me-plus"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="relative px-4 py-2 text-sm font-mono transition-all duration-300 group text-gray-700 hover:text-black ml-8 border-l border-gray-300 pl-4"
               >
                 <span className="absolute inset-0 bg-black scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
-                <span className="relative z-10 group-hover:text-white">[github]</span>
+                <span className="relative z-10 group-hover:text-white">[source code]</span>
               </a>
             </div>
           </nav>
@@ -99,12 +99,12 @@ const Header = () => {
                 
                 <div className="pt-4 mt-4 border-t border-gray-200">
                   <a
-                    href="https://github.com/thedhanawada"
+                    href="https://github.com/thedhanawada/me-plus"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-sm text-gray-400 font-mono"
                   >
-                    github
+                    source code
                   </a>
                 </div>
               </div>
